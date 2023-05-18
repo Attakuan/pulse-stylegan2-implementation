08c45e6cd21d007c5cae0a67054ba0f6376a2e30
Emirhan Koc <93924341+kocemir@users.noreply.github.com>
2023-05-18 21:22:57 +0300

    Update PULSE.py

--- PULSE.py
+++ PULSE.py
@@ -9,7 +9,7 @@ from stylegan2 import Generator
 from drive import open_url
 
 class PULSE(torch.nn.Module):
-    def __init__(self, cache_dir, verbose=True):
+    def __init__(self, cache_dir, verbose=True, pt_path):
         super(PULSE, self).__init__()
 
         self.verbose = verbose
@@ -19,7 +19,7 @@ class PULSE(torch.nn.Module):
         cache_dir.mkdir(parents=True, exist_ok = True)
         if self.verbose: print("Loading StyleGAN2 Network")
        
-        gen_pretrained = torch.load('https://drive.google.com/uc?id=1fs6FJSf0v60tmJaljNfSwbwl2bGHd2Qq', map_location='gpu')
+        gen_pretrained = torch.load(pt_path, map_location='gpu')
         self.generator.load_state_dict(gen_pretrained, strict=True)
         print("StyleGAN2 succesfully loaded")
 
